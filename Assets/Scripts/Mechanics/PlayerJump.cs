9f33c9dfe21090af74117fbf55784af08fda60d0
jensgholm15@gmail.com <jensgholm15@gmail.com>
2017-09-21 20:42:06 +0200

    Refactored player movement system

---
+++ Assets/Scripts/Mechanics/PlayerJump.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    float m_JumpForce;
+
+    PlayerMovement m_PlayerMovement;
+
+    Rigidbody m_Body;
+
+    bool isJumping;
+
+    public void OnEnable()
+    {
+        m_PlayerMovement = GetComponent<PlayerMovement>();
+        m_Body = GetComponent<Rigidbody>();
+    }
+
+    public void Jump()
+    {
+        if (m_PlayerMovement.cooldown)
+        {
+            return;
+        }
+
+        m_PlayerMovement.cooldown = true;
+
+        isJumping = true;
+
+        m_Body.velocity = Vector3.up * m_JumpForce;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isJumping && collision.gameObject.GetComponent<Floor>())
+        {
+            isJumping = false;
+            m_PlayerMovement.cooldown = false;
+        }
+    }
+}
--- Assets/Scripts/Mechanics/PlayerMovement.cs
+++ Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -4,70 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody m_Body;
-
-    [SerializeField]
-    float m_JumpForce;
-
-    [SerializeField]
-    float m_ActionCooldownTime;
-
-    bool m_Cooldown;
-
-    IEnumerator m_ActionCooldown;
+    [HideInInspector]
+    public bool cooldown;
 
     void Start()
     {
-        m_Body = GetComponent<Rigidbody>();
-        m_ActionCooldown = ActioCooldown();
-        m_Cooldown = false;
-    }
-
-    public void Jump()
-    {
-        if (!m_Cooldown)
-        {
-            return;
-        }
-
-        m_Body.velocity = Vector3.up * m_JumpForce;
-
-        StartCoroutine(m_ActionCooldown);
-    }
-
-    public void Slide()
-    {
-        if (!m_Cooldown)
-        {
-            return;
-        }
-    }
-
-    public void Hollow()
-    {
-        if (!m_Cooldown)
-        {
-            return;
-        }
-    }
-
-    IEnumerator ActioCooldown()
-    {
-        m_Cooldown = true;
-
-        yield return new WaitForSeconds(m_ActionCooldownTime);
-
-        m_Cooldown = false;
-
-        yield return null;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.GetComponent<Floor>())
-        {
-            StopCoroutine(m_ActionCooldownTime);
-            m_Cooldown = false;
-        }
+        cooldown = false;
     }
 }
