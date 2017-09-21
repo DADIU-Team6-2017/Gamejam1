e808ea0587bbfce07d53430ee3901e9225332755
jensgholm15@gmail.com <jensgholm15@gmail.com>
2017-09-21 16:15:11 +0200

    Add initial player movement

---
+++ Assets/Scripts/Mechanics/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody m_Body;
+
+    [SerializeField]
+    float m_JumpForce;
+    
+    bool m_IsMoving;
+
+    IEnumerator m_ActionCooldown;
+
+    void Start()
+    {
+        m_Body = GetComponent<Rigidbody>();
+        m_ActionCooldown = null;
+    }
+
+    public void Jump()
+    {
+        if (m_IsMoving)
+        {
+            return;
+        }
+
+        m_Body.velocity = Vector3.up * m_JumpForce;
+    }
+
+    public void Slide()
+    {
+        if (m_IsMoving)
+        {
+            return;
+        }
+    }
+
+    public void Hollow()
+    {
+        if (m_IsMoving)
+        {
+            return;
+        }
+    }
+
+}
