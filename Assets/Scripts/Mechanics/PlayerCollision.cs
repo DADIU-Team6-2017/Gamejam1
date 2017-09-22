27b6452b09b61b56224e9dadee63258179ced0f1
Jens Thomsen <dnamesnej@gmail.com>
2017-09-22 11:17:25 +0200

    Added PlayerCollision and ordered spawn for obstacles

---
+++ Assets/Scripts/Mechanics/PlayerCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer("DuckLayer"))
+		{
+			Debug.Log("Touched duckLayer");
+		}else if(collision.collider.gameObject.layer == LayerMask.NameToLayer("JumpLayer"))
+		{
+				Debug.Log("Touched JumpLayer");
+		}else if(collision.collider.gameObject.layer == LayerMask.NameToLayer("PhaseLayer"))
+		{
+				Debug.Log("Touched PhaseLayer");
+		}
+	}
+		
+}
